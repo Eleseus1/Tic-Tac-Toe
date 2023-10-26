ed6c987aed2810f38b56fe6fc83c23010009d96e
Eleseus1 <erik.e.kothe@gmail.com>
2023-10-26 13:10:32 +0200

    The game is done and I hope there are no bugs,

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import menu as m
 import sys as s
 
 print("Welcome to Tic Tac Toe")
-print("")
-print("[1]Play\n[2]Stats\n[3]Exit")
-choice = input("(1/2/3): ")
-if choice == "1":
-    while v.restart_choice == "y" or "Y":
-        while v.winner == "":
-           m.play()
-        m.reset()
-        
-else:
-    s.exit()
+choice = "0"
+while choice not in ["1","2","3"]:
+    print("[1]Play\n[2]Stats\n[3]Exit")
+    choice = input("(1/2/3): ")
+    if choice == "1":
+        while v.restart_choice == "y" or "Y":
+            while v.winner == "":
+               m.play()
+            m.reset()
+    elif choice == "2":
+        print("")
+        m.stats_read()
+        choice = "0"
+    elif choice == "3":
+        s.exit()
+    else:
+        print("\nPlease sellect one of the options")
--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 import copy as c
 import visuals as v
 import field as f
-import time as t
+from time import localtime as time
 from sys import exit
 
 def play():
@@ -163,45 +163,45 @@ def play():
     win_check()
 
 def win_check():
-    if v._1 and v._5 and v._9 == "  x  ":
+    if v._1  == "  x  " and v._5  == "  x  " and v._9 == "  x  ":
         v.winner = "x"
-    elif v._3 and v._5 and v._7 == "  x  ":
+    elif v._3  == "  x  " and v._5  == "  x  " and v._7 == "  x  ":
         v.winner = "x"
-    elif v._1 and v._2 and v._3 == "  x  ":
+    elif v._1  == "  x  " and v._2  == "  x  " and v._3 == "  x  ":
         v.winner = "x"
-    elif v._4 and v._5 and v._6 == "  x  ":
+    elif v._4  == "  x  " and v._5  == "  x  " and v._6 == "  x  ":
         v.winner = "x"
-    elif v._7 and v._8 and v._9 == "  x  ":
+    elif v._7  == "  x  " and v._8 == "  x  " and v._9 == "  x  ":
         v.winner = "x"
-    elif v._1 and v._4 and v._7 == "  x  ":
+    elif v._1 == "  x  " and v._4  == "  x  " and v._7 == "  x  ":
         v.winner = "x"
-    elif v._2 and v._5 and v._8 == "  x  ":
+    elif v._2 == "  x  " and v._5 == "  x  " and v._8 == "  x  ":
         v.winner = "x"
-    elif v._3 and v._6 and v._9 == "  x  ":
+    elif v._3 == "  x  " and v._6 == "  x  "and v._9 == "  x  ":
         v.winner = "x"
 
-    if v._1 and v._5 and v._9 == "  o  ":
+    if v._1 == "  o  " and v._5 == "  o  " and v._9 == "  o  ":
         v.winner = "o"
-    elif v._3 and v._5 and v._7 == "  o  ":
+    elif v._3 == "  o  " and v._5 == "  o  " and v._7 == "  o  ":
         v.winner = "o"
-    elif v._1 and v._2 and v._3 == "  o  ":
+    elif v._1 == "  o  " and v._2 == "  o  " and v._3 == "  o  ":
         v.winner = "o"
-    elif v._4 and v._5 and v._6 == "  o  ":
+    elif v._4 == "  o  " and v._5 == "  o  " and v._6 == "  o  ":
         v.winner = "o"
-    elif v._7 and v._8 and v._9 == "  o  ":
+    elif v._7 == "  o  " and v._8 == "  o  " and v._9 == "  o  ":
         v.winner = "o"
-    elif v._1 and v._4 and v._7 == "  o  ":
+    elif v._1 == "  o  " and v._4 == "  o  " and v._7 == "  o  ":
         v.winner = "o"
-    elif v._2 and v._5 and v._8 == "  o  ":
+    elif v._2 == "  o  " and v._5 == "  o  " and v._8 == "  o  ":
         v.winner = "o"
-    elif v._3 and v._6 and v._9 == "  o  ":
+    elif v._3 == "  o  " and v._6 == "  o  " and v._9 == "  o  ":
         v.winner = "o"
 
     if v.winner == "x":
         print("Player x wins this match")
     elif v.winner == "o":
         print("Player o wins this match")
-    elif v._1 and v._2 and v._3 and v._4 and v._5 and v._6 and v._7 and v._8 and v._9 != v.empty:
+    elif v._1 != v.empty and v._2 != v.empty and v._3 != v.empty and v._4 != v.empty and v._5 != v.empty and v._6 != v.empty and v._7 != v.empty and v._8 != v.empty and v._9 != v.empty:
         v.winner = "draw"
         print("It`s a draw")
 
@@ -212,11 +212,10 @@ def reset():
         v.o_wins +=1
     elif v.winner == "draw":
         v.draw += 1
-    print(f"Player x`s wins:{v.x_wins}")
-    print(f"Player o`s wins:{v.o_wins}")
-    print(f"Draws: {v.draw}")
+    total_wins = (f"Player x`s wins: {v.x_wins}\nPlayer o`s wins: {v.o_wins}\nDraws: {v.draw}\n")
+    print(total_wins)
     v.restart_choice = input("Do you want to play again?[Y/n] ")
-    if v.restart_choice == "y" or "Y":
+    if v.restart_choice == "y" and "Y":
         v._1 = v.empty
         v._2 = v.empty
         v._3 = v.empty
@@ -226,12 +225,31 @@ def reset():
         v._7 = v.empty
         v._8 = v.empty
         v._9 = v.empty
-
-        v.turn = "x"
+        if v.x_wins < v.o_wins:
+            v.turn = "x"
+        elif v.o_wins < v.x_wins:
+            v.turn = "o"
         v.winner = ""
         f.field = c.deepcopy(f.original_field)
     else:
+        stats_write(total_wins)
         exit()
+    return total_wins
+
+def stats_write(total_wins):
+    date = time()
+    date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+str(date[3])+":"+str(date[4])+"\n"
+    wins_check = v.x_wins + v.o_wins + v.draw
+    if wins_check > 0:
+        with open("ttt_stats.txt","a") as s:
+            s.write(date+total_wins+"\n")
+
+def stats_read():
+    with open("ttt_stats.txt","+r") as s:
+        s.write("")
+        stats = s.read()
+    print(stats)
+
 """
 play()
 print(f.field)
