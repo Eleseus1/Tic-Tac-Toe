98332fad357d756086fee0187cc0304c53a897ab
Eleseus1 <erik.e.kothe@gmail.com>
2023-11-25 10:25:28 +0100

    I made some progres with the easy bot

--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 import copy as c
 import visuals as v
 import field as f
-from random import choice as random_choice
+import random as r
 from time import localtime as time
 from time import sleep
 from sys import exit
@@ -59,258 +59,291 @@ def play():
         if choice == "1" and v._1 == v.empty:
             if v.turn == "x":
                 v._1 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._1 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         elif choice == "2" and v._2 == v.empty:
             if v.turn == "x":
                 v._2 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._2 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         elif choice == "3" and v._3 == v.empty:
             if v.turn == "x":
                 v._3 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._3 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         elif choice == "4" and v._4 == v.empty:
             if v.turn == "x":
                 v._4 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._4 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         elif choice == "5" and v._5 == v.empty:
             if v.turn == "x":
                 v._5 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._5 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         elif choice == "6" and v._6 == v.empty:
             if v.turn == "x":
                 v._6 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._6 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         elif choice == "7" and v._7 == v.empty:
             if v.turn == "x":
                 v._7 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._7 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         elif choice == "8" and v._8 == v.empty:
             if v.turn == "x":
                 v._8 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._8 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         elif choice == "9" and v._9 == v.empty:
             if v.turn == "x":
                 v._9 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
             elif v.turn == "o":
                 v._9 = c.deepcopy(v.o)
-                f.field = f.update_field()
                 v.turn = "x"
                 break
 
         else:
             print("Please sellect one of the options")
+    f.field = f.update_field()
     print(f.field)
     win_check()
 
 def play_easy():
     if v.start_time == "":
         v.start_time = time()
-        v.start_time = str(v.start_time[3])+":"+str(v.start_time[4])
+        v.start_time = str(v.start_time[3])+":"+str(v.start_time[4]) # This saves the start time for the stats
+    field_list = []
     # This checks if the squares are empty
     if v._1 == v.empty:
         choice_1 = "1"
+        field_list.append(v._1)
     else:
         choice_1 = "/"
     if v._2 == v.empty:
         choice_2 = "2"
+        field_list.append(v._2)
     else:
         choice_2 = "/"
     if v._3 == v.empty:
         choice_3 = "3"
+        field_list.append(v._3)
     else:
         choice_3 = "/"
 
     if v._4 == v.empty:
         choice_4 = "4"
+        field_list.append(v._4)
     else:
         choice_4 = "/"
     if v._5 == v.empty:
         choice_5 = "5"
+        field_list.append(v._5)
     else:
         choice_5 = "/"
     if v._6 == v.empty:
         choice_6 = "6"
+        field_list.append(v._6)
     else:
         choice_6 = "/"
 
     if v._7 == v.empty:
         choice_7 = "7"
+        field_list.append(v._7)
     else:
         choice_7 = "/"
     if v._8 == v.empty:
         choice_8 = "8"
+        field_list.append(v._8)
     else:
         choice_8 = "/"
     if v._9 == v.empty:
         choice_9 = "9"
+        field_list.append(v._9)
     else:
         choice_9 = "/"
     
     choice = ""
+    #The players turn
     while choice not in ["1", "2", "3", "5", "6", "7", "8", "9"]:
         print("")
         choice = input(f"Player {v.turn}'s turn\n{choice_1}|{choice_2}|{choice_3}\n{choice_4}|{choice_5}|{choice_6}\n{choice_7}|{choice_8}|{choice_9}\nChoose a square: ")
         
         if choice == "1" and v._1 == v.empty and v.turn == "x":
                 v._1 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         elif choice == "2" and v._2 == v.empty and v.turn == "x":
                 v._2 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         elif choice == "3" and v._3 == v.empty and v.turn == "x":
                 v._3 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         elif choice == "4" and v._4 == v.empty and v.turn == "x":
                 v._4 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         elif choice == "5" and v._5 == v.empty and v.turn == "x":
                 v._5 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         elif choice == "6" and v._6 == v.empty and v.turn == "x":
                 v._6 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         elif choice == "7" and v._7 == v.empty and v.turn == "x":
                 v._7 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         elif choice == "8" and v._8 == v.empty and v.turn == "x":
                 v._8 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         elif choice == "9" and v._9 == v.empty and v.turn == "x":
                 v._9 = c.deepcopy(v.x)
-                f.field = f.update_field()
                 v.turn = "o"
                 break
 
         else:
             print("Please sellect one of the options")
+    f.field = f.update_field()
     print(f.field)
     win_check()
+    field_list = []
+    # This checks if the squares are empty
+    if v._1 == v.empty:
+        choice_1 = "1"
+        field_list.append(v._1)
+    else:
+        choice_1 = "/"
+    if v._2 == v.empty:
+        choice_2 = "2"
+        field_list.append(v._2)
+    else:
+        choice_2 = "/"
+    if v._3 == v.empty:
+        choice_3 = "3"
+        field_list.append(v._3)
+    else:
+        choice_3 = "/"
+
+    if v._4 == v.empty:
+        choice_4 = "4"
+        field_list.append(v._4)
+    else:
+        choice_4 = "/"
+    if v._5 == v.empty:
+        choice_5 = "5"
+        field_list.append(v._5)
+    else:
+        choice_5 = "/"
+    if v._6 == v.empty:
+        choice_6 = "6"
+        field_list.append(v._6)
+    else:
+        choice_6 = "/"
+
+    if v._7 == v.empty:
+        choice_7 = "7"
+        field_list.append(v._7)
+    else:
+        choice_7 = "/"
+    if v._8 == v.empty:
+        choice_8 = "8"
+        field_list.append(v._8)
+    else:
+        choice_8 = "/"
+    if v._9 == v.empty:
+        choice_9 = "9"
+        field_list.append(v._9)
+    else:
+        choice_9 = "/"
     o_turn_end = False
-    field_list = [v._1, v._2, v._3, v._4, v._5, v._6, v._7, v._8, v._9,]
     sleep(1)
     while o_turn_end == False:
-        field_o = random_choice(field_list)
-        if field_o == v.empty:
+        v._2 = "2"
+        print(f"{field_list}<-list")
+        r.shuffle(field_list)
+        print(f"{field_list}<-shuffle")
+        field_o = "draw"
+        if len(field_list) > 0:
+            field_o = field_list[0]
+            print(f"{field_list}<-[0]")
+        if field_o != 1: # <-- This is because im to lazy to remove the whitespaces
             if field_o == v._1:
                 v._1 = c.deepcopy(v.o)
-                f.field = f.update_field()
             elif field_o == v._2:
                 v._2 = c.deepcopy(v.o)
-                f.field = f.update_field()
             elif field_o == v._3:
                 v._3 = c.deepcopy(v.o)
-                f.field = f.update_field()
             elif field_o == v._4:
                 v._4 = c.deepcopy(v.o)
-                f.field = f.update_field()
             elif field_o == v._5:
                 v._5 = c.deepcopy(v.o)
-                f.field = f.update_field()
             elif field_o == v._6:
                 v._6 = c.deepcopy(v.o)
-                f.field = f.update_field()
             elif field_o == v._7:
                 v._7 = c.deepcopy(v.o)
-                f.field = f.update_field()
             elif field_o == v._8:
                 v._8 = c.deepcopy(v.o)
-                f.field = f.update_field()
             elif field_o == v._9:
                 v._9 = c.deepcopy(v.o)
-                f.field = f.update_field()
             v.turn = "x"
             o_turn_end = True
+    f.field = f.update_field()
     print("\nEasy Bots turn:")
     print(f.field)
     win_check()
@@ -356,7 +389,7 @@ def win_check():
         print("Player x wins this match")
     elif v.winner == "o":
         print("Player o wins this match")
-#    elif v._1 != v.empty and v._2 != v.empty and v._3 != v.empty and v._4 != v.empty and v._5 != v.empty and v._6 != v.empty and v._7 != v.empty and v._8 != v.empty and v._9 != v.empty:
+    # elif v._1 != v.empty and v._2 != v.empty and v._3 != v.empty and v._4 != v.empty and v._5 != v.empty and v._6 != v.empty and v._7 != v.empty and v._8 != v.empty and v._9 != v.empty:
     elif v.empty not in [v._1,v._2,v._3,v._4,v._5,v._6,v._7,v._8,v._9]:
         v.winner = "draw"
         print("It`s a draw")
@@ -400,24 +433,24 @@ def stats_write(total_wins):
     elif v.gametype == "player":
         player_x = input("Player x`s name: ")
         player_o = input("Player o`s name: ")
-        date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+v.start_time+"-"+str(date[3])+": "+str(date[4])+player_x+" vs. "+player_o+"\n"
+        date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+v.start_time+"-"+str(date[3])+": "+str(date[4])+" "+player_x+" vs. "+player_o+"\n"
     
     elif v.gametype == "easy":
         player_x = input("Player x`s name: ")
-        date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+v.start_time+"-"+str(date[3])+": "+str(date[4])+player_x+" vs. "+"Easy Bot"+"\n"
+        date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+v.start_time+"-"+str(date[3])+": "+str(date[4])+" "+player_x+" vs. "+"Easy Bot"+"\n"
     
     elif v.gametype == "medium":
         player_x = input("Player x`s name: ")
-        date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+v.start_time+"-"+str(date[3])+": "+str(date[4])+player_x+" vs. "+"Medium Bot"+"\n"
+        date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+v.start_time+"-"+str(date[3])+": "+str(date[4])+" "+player_x+" vs. "+"Medium Bot"+"\n"
     
 
     elif v.gametype == "undefeatable":
         player_x = input("Player x`s name: ")
-        date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+v.start_time+"-"+str(date[3])+": "+str(date[4])+player_x+" vs. "+"Undefeatable Bot"+"\n"
+        date = str(date[2]) +", "+ str(date[1]) +", "+ str(date[0]) +" "+v.start_time+"-"+str(date[3])+": "+str(date[4])+" "+player_x+" vs. "+"Undefeatable Bot"+"\n"
 
     if v.games > 0:
         with open("ttt_stats.txt","a") as s:
-            s.write(date+total_wins+"\n")
+            s.write(str(date)+str(total_wins)+"\n")
 
 def stats_read():
 
